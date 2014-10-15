6d73ef3138d91bc50eaf2bc4c8b3d0a303154a70
Gary Bake <garybake@gmail.com>
2014-10-15 13:47:25 +0100

    Inital play with json services

--- server.js
+++ server.js
@@ -1,10 +1,20 @@
 var express = require('express');
 var app = express();
+var bodyParser = require('body-parser');
+
+app.use(bodyParser.urlencoded({ extended: false }));
+
+// parse application/json
+app.use(bodyParser.json());
 
 app.set('port', process.env.PORT || 3000);
 
 app.use(express.static(__dirname + '/static'));
 app.use('/bower_components',  express.static(__dirname + '/bower_components'));
+
+// app.configure(function(){
+//   app.use(express.bodyParser());
+// });
 
 var ips  = [
     { id: 0, date: '11/10/2014', ip: '10.11.12.13' },
@@ -13,6 +23,15 @@ var ips  = [
 
 app.get('/api/test', function(req, res){
     res.json(ips);
+});
+
+app.post('/api/wines', function(req, res) {
+    console.log(req.body);
+    res.send([{name:'wine1'}, {name:'wine2'}]);
+});
+
+app.post('/api/wines/:id', function(req, res) {
+    res.send({id:req.params.id, name: "The Name", description: "description"});
 });
 
 // Custom 404
