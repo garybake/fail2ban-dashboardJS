b90da54c2a35ef7473efaeea4b9757311c8f71fb
Gary Bake <garybake@gmail.com>
2014-11-27 12:27:12 +0000

    Added basic read services

---
+++ config.js
@@ -0,0 +1,7 @@
+module.exports = {
+    f2b: {
+        confFile: './testdata/fail2ban.conf',
+        logFile: './testdata/fail2ban.log',
+        jailConfFile: './testdata/jail.conf'
+    }
+};
--- server.js
+++ server.js
@@ -1,38 +1,63 @@
-var express = require('express');
+var express = require('express'),
+    fs = require("fs"),
+    config = require('./config.js'),
+    ini = require('ini'),
+    exec = require('child_process').exec;
 var app = express();
-var bodyParser = require('body-parser');
-
-app.use(bodyParser.urlencoded({ extended: false }));
-
-// parse application/json
-app.use(bodyParser.json());
 
 app.set('port', process.env.PORT || 3000);
 
 app.use(express.static(__dirname + '/static'));
 app.use('/bower_components',  express.static(__dirname + '/bower_components'));
 
-// app.configure(function(){
-//   app.use(express.bodyParser());
+app.post('/api/appstatus', function(req, res){
+    var status = {status: "ok"};
+    res.json(status);
+});
+
+app.post('/api/serverstatus', function(req, res){
+    var command = 'fail2ban-server status';
+    exec(command, function(error, stdout, stderr){
+        // console.log(stdout);
+        res.json({status: stdout});
+    });
+    // res.json({status: "sss"});
+});
+
+app.post('/api/log', function(req, res) {
+    var eventsList = {events: []};
+
+    fs.readFile(config.f2b.logFile, "utf8", function (err, data) {
+        if (err) throw err;
+
+        var lines = data.split("\n");
+        lines.forEach(function(line){
+            var logLine = logLineToEvent(line);
+            if (logLine.type === 'Ban'){
+                eventsList.events.push(logLineToEvent(line));
+            }
+        });
+        res.json(eventsList);
+    });
+});
+
+app.post('/api/conf', function(req, res) {
+    var conFile = config.f2b.jailConfFile;
+    var configData = ini.parse(fs.readFileSync(conFile, 'utf-8'));
+
+    res.json(configData);
+});
+
+
+
+// app.post('/api/wines', function(req, res) {
+//     console.log(req.body);
+//     res.send([{name:'wine1'}, {name:'wine2'}]);
 // });
 
-var ips  = [
-    { id: 0, date: '11/10/2014', ip: '10.11.12.13' },
-    { id: 2, date: '11/10/2014', ip: '14.15.16.17' },
-];
-
-app.get('/api/test', function(req, res){
-    res.json(ips);
-});
-
-app.post('/api/wines', function(req, res) {
-    console.log(req.body);
-    res.send([{name:'wine1'}, {name:'wine2'}]);
-});
-
-app.post('/api/wines/:id', function(req, res) {
-    res.send({id:req.params.id, name: "The Name", description: "description"});
-});
+// app.post('/api/wines/:id', function(req, res) {
+//     res.send({id:req.params.id, name: "The Name", description: "description"});
+// });
 
 // Custom 404
 app.use(function(req, res){
@@ -51,3 +76,19 @@ app.listen(app.get('port'), function(){
     console.log('Server started on http://localhost:' +
         app.get('port') + '; press Ctrl-C to terminate');
 });
+
+
+function logLineToEvent(line){
+    var eventObj = {};
+    if (line.indexOf('Ban') != -1){
+        eventObj.type = 'Ban';
+        eventObj.time = line.substr(0, 19);
+        var lineDetail = line.substr(42, line.length);
+        eventObj.service = lineDetail.substring(lineDetail.lastIndexOf('[')+1, lineDetail.lastIndexOf(']'));
+        eventObj.ip = lineDetail.split(' ')[3];
+    } else {
+        // TODO implement other log events
+        eventObj.type = 'Other';
+    }
+    return eventObj;
+}
